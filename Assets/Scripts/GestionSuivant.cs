03676633ffd781bbfcb9d5ace3aae6a4963369dd
Ryanb0b <ryanbob2005@gmail.com>
2024-12-17 13:37:05 -0500

    creation des scene perdu et gagne

---
+++ Assets/Scripts/GestionSuivant.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GestionSuivant : MonoBehaviour
+{
+
+    public void ChangerSceneFin(){
+        SceneManager.LoadScene("Fin");
+    }
+}
+
